d5738f1cdae2ca9662343c2600a139ce8e1505e5
holmes27 <homiraghuvanshi27@gmail.com>
2021-07-12 22:34:12 +0530

    Submission for  Microsoft Engage 2021

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
+
+app.set("view engine", "ejs"); // To tell Express EJS is used
 const io = require("socket.io")(server, {
   cors: {
     origin: '*'
@@ -45,7 +46,7 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-
+//When someone connects to the server
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
@@ -54,6 +55,7 @@ io.on("connection", (socket) => {
       io.to(roomId).emit("createMessage", message, userName);
 
     });
+    // To communicate the disconnection
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
       
